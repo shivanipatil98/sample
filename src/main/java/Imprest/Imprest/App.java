d14767493b5e5dd13bf0135f17baed3f84d8de36
shivani <shivani.patil@digitelescope.com>
2024-04-18 19:42:37 +0530

    first commit

---
+++ src/main/java/Imprest/Imprest/App.java
@@ -0,0 +1,13 @@
+package Imprest.Imprest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
